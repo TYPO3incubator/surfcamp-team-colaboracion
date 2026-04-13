db4b34815272724e27d37404f64b9d8955014429
Lisa Kreitz <lisa.kreitz@b13.com>
2026-04-13 10:13:38 +0100

    ModuleController wihout Extbase

--- packages/collaboration/Configuration/Backend/Modules.php
+++ packages/collaboration/Configuration/Backend/Modules.php
@@ -4,14 +4,17 @@ use TYPO3Incubator\Collaboration\Backend\Controller\ModuleController;
 
 return [
     'sse_test' => [
+        'parent' => 'content',
         'position' => ['after' => 'web_FormFormbuilder'],
         'access' => 'user',
         'path' => '/module/web/example',
         'labels' => 'SSE Test',
         'extensionName' => 'Collaboration',
         'iconIdentifier' => 'tx_examples-backend-module',
-        'controllerActions' => [
-            ModuleController::class => ['event'],
+        'routes' => [
+            '_default' => [
+                'target' => ModuleController::class . '::handleRequest',
+            ],
         ],
     ],
 ];
