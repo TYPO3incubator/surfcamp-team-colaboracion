1bf4bcd20d3149bc33c7235be247678e51a6b287
lars <scherbel@quellwerke.de>
2026-04-15 15:35:08 +0100

    fix: remove unused imports

--- packages/collaboration/Classes/EventListener/PageRendererEventListener.php
+++ packages/collaboration/Classes/EventListener/PageRendererEventListener.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace TYPO3Incubator\Collaboration\EventListener;
 
-use TYPO3\CMS\Backend\Controller\Event\AfterBackendPageRenderEvent;
 use TYPO3\CMS\Backend\Controller\Event\BeforeBackendPageRenderEvent;
 use TYPO3\CMS\Core\Attribute\AsEventListener;
 use TYPO3\CMS\Core\Page\JavaScriptModuleInstruction;
-use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Redirects\EventListener\AfterBackendPageRendererEventListener;
 
 #[AsEventListener]
 class PageRendererEventListener
